3f0f64be1c8f7f8677add90983f1880cc6e640ce
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-15 16:52:09 +0800

    Modified to add OkHttpClient

--- app/src/main/java/com/shareem/myapplication/network/RetrofitInstance.java
+++ app/src/main/java/com/shareem/myapplication/network/RetrofitInstance.java
@@ -1,16 +1,23 @@
 package com.shareem.myapplication.network;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://api.myjson.com/";
-
+    private static final String BASE_URL = "http://172.16.0.76:8081/";
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS);
     public static Retrofit getRetrofitInstance() {
         if(retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(httpClient.build())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
