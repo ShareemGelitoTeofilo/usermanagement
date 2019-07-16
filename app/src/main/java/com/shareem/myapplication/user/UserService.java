c0be29ad98d9d0684ce74fe269fb736d09a7a6c3
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:35:16 +0800

    Added an endpoint for accessing a web service for signing up a user

--- app/src/main/java/com/shareem/myapplication/user/UserService.java
+++ app/src/main/java/com/shareem/myapplication/user/UserService.java
@@ -3,12 +3,17 @@ package com.shareem.myapplication.user;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface UserService {
     @GET("users/login")
     Call<User> loginUser(@Query("username") String username, @Query("password") String password);
+
+    @POST("users/signUp")
+    Call<User> signUpUser(@Body User user);
 
     @GET("users/findAll")
     Call<List<User>> findAllUsers();
