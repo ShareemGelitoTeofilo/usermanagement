a150f53957fbae0694405ff32601c5e9bf761886
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-18 16:25:22 +0800

    Created UserDao

---
+++ app/src/main/java/com/shareem/myapplication/user/UserDao.java
@@ -0,0 +1,28 @@
+package com.shareem.myapplication.user;
+
+import io.realm.Realm;
+
+public class UserDao {
+
+    public User insert(User user){
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        UserRO userRO = UserMapper.toUserRO(user);
+        userRO = realm.copyToRealm(userRO);
+        user = UserMapper.toUser(userRO);
+        realm.commitTransaction();
+        realm.close();
+        return user;
+    }
+
+    public User update(User user){
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        UserRO userRO = UserMapper.toUserRO(user);
+        userRO = realm.copyToRealmOrUpdate(userRO);
+        user = UserMapper.toUser(userRO);
+        realm.commitTransaction();
+        realm.close();
+        return user;
+    }
+}
