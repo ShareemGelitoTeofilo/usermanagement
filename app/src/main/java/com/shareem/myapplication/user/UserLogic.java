2a63a076a776e62a53bc5d93407c851e7b087472
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-18 12:56:34 +0800

    Added a login functionality

--- app/src/main/java/com/shareem/myapplication/user/UserLogic.java
+++ app/src/main/java/com/shareem/myapplication/user/UserLogic.java
@@ -1,13 +1,30 @@
 package com.shareem.myapplication.user;
 
+import com.shareem.myapplication.AppCallback;
+import com.shareem.myapplication.loginhistory.LoginHistory;
+import com.shareem.myapplication.loginhistory.LoginHistoryDao;
+import com.shareem.myapplication.loginhistory.LoginHistoryFactory;
+import com.shareem.myapplication.loginhistory.LoginHistoryMapper;
+import com.shareem.myapplication.loginhistory.LoginHistoryRO;
+import com.shareem.myapplication.network.RetrofitInstance;
+
 import java.util.List;
+
+import io.realm.RealmResults;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserLogic {
 
     private static UserLogic userLogicInstance;
-    private LoginHistoryDao loginHistoryDao = new LoginHistoryDao();
+    private LoginHistoryDao loginHistoryDao;
+    private UserService userService;
 
-    private UserLogic() {}
+    private UserLogic() {
+        userService = RetrofitInstance.getRetrofitInstance().create(UserService.class);
+        loginHistoryDao = new LoginHistoryDao();
+    }
 
     public static UserLogic getInstance(){
         if(userLogicInstance == null){
@@ -22,7 +39,25 @@ public class UserLogic {
     }
 
     public List<LoginHistory> getLoginHistoriesByUsername(String username){
-        return loginHistoryDao.findAllByUsername(username);
+        List<LoginHistory> loginHistories = loginHistoryDao.findAllByUsername(username);
+        return loginHistories;
+    }
+
+    public void loginUser(String username, String password, final AppCallback callback) {
+        Call<User> userCall = userService.loginUser(username, password);
+        userCall.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                createLoginHistoryForUser(user);
+                callback.onCallback(user, "Logged in");
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                callback.onCallback(null, "");
+            }
+        });
     }
 
 
