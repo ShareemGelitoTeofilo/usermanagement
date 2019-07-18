38c30bce1966bed98bb9699dc6a11037a93b7a52
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-18 09:16:18 +0800

    deleted

--- app/src/main/java/com/shareem/myapplication/friend/FriendFactory.java
+++
@@ -1,7 +0,0 @@
-package com.shareem.myapplication.friend;
-
-public class FriendFactory {
-    public static Friend create(String name, String email, String address){
-        return new Friend(name, email,address);
-    }
-}
